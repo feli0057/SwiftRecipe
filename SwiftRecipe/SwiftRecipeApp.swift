ce1b90d3125cf8b06021f0384db3792273589f06
Reggie Felix <feli0057@algonquinlive.com>
2024-10-31 23:09:10 -0400

    Initial Commit

---
+++ SwiftRecipe/SwiftRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftRecipeApp.swift
+//  SwiftRecipe
+//
+//  Created by Reginald Felix on 2024-10-31.
+//
+
+import SwiftUI
+
+@main
+struct SwiftRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
